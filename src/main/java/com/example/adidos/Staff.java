a873d7c63c501b7b05742396d32d363d49603be2
Hidayah Saharudin <2020847026@student.uitm.edu.my>
2022-07-19 23:10:50 +0800

    Commit Staff javabean and StaffDao

---
+++ src/main/java/com/example/adidos/Staff.java
@@ -0,0 +1,134 @@
+package com.example.adidos;
+
+import java.sql.Date;
+
+public class Staff {
+	private String staffID;
+	private String staffName;
+	private String staffPhone;
+	private String staffEmail;
+	private Date staffBirthDate;
+	private String staffAddress; 
+	private String staffCity; 
+	private String staffPostcode; 
+	private String staffState;
+	private String staffGender;
+	private String staffNationality;
+	private String staffMaritalStatus;
+	private String staffReligion;
+	private String staffPassword;
+
+	public Staff(){}
+
+	public String getStaffID() {
+		return staffID;
+	}
+
+	public void setStaffID(String staffID) {
+		this.staffID = staffID;
+	}
+
+	public String getStaffName() {
+		return staffName;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+
+	public String getStaffPhone() {
+		return staffPhone;
+	}
+
+	public void setStaffPhone(String staffPhone) {
+		this.staffPhone = staffPhone;
+	}
+
+	public String getStaffEmail() {
+		return staffEmail;
+	}
+
+	public void setStaffEmail(String staffEmail) {
+		this.staffEmail = staffEmail;
+	}
+
+	public Date getStaffBirthDate() {
+		return staffBirthDate;
+	}
+
+	public void setStaffBirthDate(Date staffBirthDate) {
+		this.staffBirthDate = staffBirthDate;
+	}
+
+	public String getStaffAddress() {
+		return staffAddress;
+	}
+
+	public void setStaffAddress(String staffAddress) {
+		this.staffAddress = staffAddress;
+	}
+
+	public String getStaffCity() {
+		return staffCity;
+	}
+
+	public void setStaffCity(String staffCity) {
+		this.staffCity = staffCity;
+	}
+
+	public String getStaffPostcode() {
+		return staffPostcode;
+	}
+
+	public void setStaffPostcode(String staffPostcode) {
+		this.staffPostcode = staffPostcode;
+	}
+
+	public String getStaffState() {
+		return staffState;
+	}
+
+	public void setStaffState(String staffState) {
+		this.staffState = staffState;
+	}
+
+	public String getStaffGender() {
+		return staffGender;
+	}
+
+	public void setStaffGender(String staffGender) {
+		this.staffGender = staffGender;
+	}
+
+	public String getStaffNationality() {
+		return staffNationality;
+	}
+
+	public void setStaffNationality(String staffNationality) {
+		this.staffNationality = staffNationality;
+	}
+
+	public String getStaffMaritalStatus() {
+		return staffMaritalStatus;
+	}
+
+	public void setStaffMaritalStatus(String staffMaritalStatus) {
+		this.staffMaritalStatus = staffMaritalStatus;
+	}
+
+	public String getStaffReligion() {
+		return staffReligion;
+	}
+
+	public void setStaffReligion(String staffReligion) {
+		this.staffReligion = staffReligion;
+	}
+
+	public String getStaffPassword() {
+		return staffPassword;
+	}
+
+	public void setStaffPassword(String staffPassword) {
+		this.staffPassword = staffPassword;
+	}
+}
---
+++ src/main/java/com/example/adidos/StaffDao.java
@@ -0,0 +1,5 @@
+package com.example.adidos;
+
+public class StaffDao {
+
+}
